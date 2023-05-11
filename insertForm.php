16666d91024782b3082e70f9a4772f9155b27129
Yahikopnj <benjamin.martel@live.fr>
2023-05-11 11:58:04 +0200

    premier formulaire

---
+++ insertForm.php
@@ -0,0 +1,11 @@
+<?php
+echo 'nom : '.$_POST['nom'].'<br>'."bjr ";
+echo 'prenom : '.$_POST["prenom"].'<h1>'.'yoyo ';
+echo 'email : '.$_POST["email"].'<pre>'.'ekip ';
+echo 'mdp : '.$_POST["mdp"].'<>'.'wave ';
+echo 'date : '.$_POST["date"].'<br>'.'wexe ';
+echo 'rgpd : '.$_POST["rgpd"].'<br>';
+
+
+
+?>
